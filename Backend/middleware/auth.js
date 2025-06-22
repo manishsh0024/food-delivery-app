768a0b78a1fff0d294badf9896477de756e8a642
Manish Sharma <somecreative911674@gmail.com>
2025-06-22 15:13:51 +0530

    backend updated

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 
 
-const authMiddleware = async (req, res, next) => {
+const authMiddleware = async (req, res, next) => { 
     const {token} = req.headers;
     if(!token) {
         return res.status(401).json({ success:false ,message: 'Unauthorized access, Login first' });
