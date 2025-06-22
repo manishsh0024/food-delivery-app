285cbb57f768c4cb4999b4d5c64446a0546194f2
Manish Sharma <somecreative911674@gmail.com>
2025-06-22 12:38:41 +0530

    add backend Url

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = ({ children }) => {
   const [cartItem, setCartItem] = useState({});
-  const url = "http://localhost:4000";
+  const url = "https://food-delivery-app-oaox.onrender.com";
   const [token, setToken] = useState("");
 
   const [food_list, setFoodList] = useState([]);
